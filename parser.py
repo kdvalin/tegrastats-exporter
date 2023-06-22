b2a422e5502c6d06c209030d87f587c6421c6eea
Keith Valin <kvalin@redhat.com>
2023-06-22 15:00:26 -0400

    Output to CSV now working

--- parser.py
+++ parser.py
@@ -1,22 +1,37 @@
 #!/usr/bin/env python3
 import sys
 import stats
+import csv
 
 def parse_file(filename):
     cont = stats.StatContainer()
     with open(filename, 'r') as f:
-        for line in f.readlines():
-            args = line.strip().split(' ')
+        lines = f.readlines()
 
-            timestamp = args[0:1]
+    first_line = True
+    keys = ["Time"]
+    for line in lines:
+        args = line.strip().split(' ')
+        timestamp = args[0:2]
+        row = [' '.join(timestamp)]
+        
+        for i in range(len(args)):
+            key = args[i]
+            stat_cont = cont.find_stat(key)
 
-            for i in range(len(args)):
-                key = args[i]
-                stat_cont = cont.find_stat(key)
+            if stat_cont is not None:
+                data = stat_cont.parse(args[i:i+stat_cont.get_num_args()])
+                if first_line:
+                    keys.extend([i[0] for i in data])
+                row.extend([i[1] for i in data])
+        with open("out.csv", "w" if first_line else "a+", newline="") as out:
+            csvwriter = csv.writer(out)
+            if first_line:
+                csvwriter.writerow(keys)
+                first_line = False
+            csvwriter.writerow(row)
+            
 
-                if stat_cont is not None:
-                    print(f"{stat_cont.get_num_args()} | {len(args[i:i+stat_cont.get_num_args()])}")
-                    print(stat_cont.parse(args[i:i+stat_cont.get_num_args()]))
 
 
 if __name__ == "__main__":
