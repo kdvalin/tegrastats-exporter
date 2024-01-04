cc14084e59670b86adf1b9afafc157aee5a6a652
Keith Valin <kvalin@redhat.com>
2024-01-04 10:30:57 -0500

    feat: Add Argument validation, if no regexes for args are specified assume none is required

--- stats/base.py
+++ stats/base.py
@@ -1,14 +1,26 @@
 import abc
 import logging
 from typing import List, Tuple
+import re
 
 class NvidiaStat:
     _logger = logging.getLogger(__package__)
     _identifier = ""
     _num_args = 0
 
+    _args_regex = []
+
     def matches(self, arg: str) -> bool:
         return self._identifier == arg
+
+    def args_match(self, args: List[str]) -> bool:
+        if len(self._args_regex) == 0: # If no arg regex specified, assume all is good
+            return True
+        
+        for (idx, arg) in enumerate(args[1:]):
+            if re.match(self._args_regex[idx], arg) is None:
+                return False
+        return True
 
     def header(self) -> str:
         return self._identifier
